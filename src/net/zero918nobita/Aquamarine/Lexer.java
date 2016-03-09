26d9a33b6fd353ddc73f3433280b53857973b946
0918nobita <nobita.0918@gmail.com>
2016-03-09 23:13:41 +0900

    Add advance() method that checks whether the next token is exist

--- src/net/zero918nobita/Aquamarine/Lexer.java
+++ src/net/zero918nobita/Aquamarine/Lexer.java
@@ -13,4 +13,23 @@ public class Lexer {
     public Lexer(Reader r) {
         reader = r;
     }
+
+    /** 次のトークンに進む
+     * @return 次のトークンがあればtrue、なければfalse
+     */
+    public boolean advance() {
+        try {
+            int c = reader.read();
+            if (c < 0) return false;
+            if (Character.isDigit((char)c)) {
+                // 数字が続く限り読み込む
+            } else {
+                throw new Exception("数字ではありません");
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 }
