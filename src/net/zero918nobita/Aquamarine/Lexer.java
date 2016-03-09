3b5a3705a674e08aa8499aad3d982906b8dadf2b
0918nobita <nobita.0918@gmail.com>
2016-03-09 21:51:07 +0900

    Enable to read text data that becomes subject to lexical analysis

--- src/net/zero918nobita/Aquamarine/Lexer.java
+++ src/net/zero918nobita/Aquamarine/Lexer.java
@@ -1,8 +1,16 @@
 package net.zero918nobita.Aquamarine;
 
+import java.io.Reader;
+
 /**
  * Created by 0918nobita on 2016/03/09.
  */
 public class Lexer {
+    public Reader reader;
 
+    /** readerはトークンの読み込み元
+     */
+    public Lexer(Reader r) {
+        reader = r;
+    }
 }
