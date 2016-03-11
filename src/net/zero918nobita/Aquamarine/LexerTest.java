f6cfe07f9e73e117362007e442aeee816f9003e3
0918nobita <nobita.0918@gmail.com>
2016-03-11 19:05:25 +0900

    Add LexerTest class to test operations of Lexer class using JUnit As far as I tried testing, token() method behaved properly, but value() method didn't behave properly. I suppose I misused read() method of Reader class.

---
+++ src/net/zero918nobita/Aquamarine/LexerTest.java
@@ -0,0 +1,30 @@
+package net.zero918nobita.Aquamarine;
+
+import java.io.Reader;
+import java.io.StringReader;
+import junit.framework.TestCase;
+
+/**
+ * Created by 0918nobita on 2016/03/11.
+ */
+public class LexerTest extends TestCase {
+    public void testToken() {
+        Reader reader = new StringReader("1234");
+        Lexer lex = new Lexer(reader);
+        if (lex.advance()) {
+            assertEquals(lex.token(), TokenType.INT);
+        } else {
+            fail();
+        }
+    }
+
+    public void testValue() {
+        Reader reader = new StringReader("1234");
+        Lexer lex = new Lexer(reader);
+        if (lex.advance()) {
+            assertEquals(lex.value(), new Integer(1234));
+        } else {
+            fail();
+        }
+    }
+}
