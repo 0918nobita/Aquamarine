b9342e0b1c99643f6f48c9bd6ed66498e16e6472
0918nobita <nobita.0918@gmail.com>
2016-03-12 21:59:10 +0900

    Add a constant to TokenType class EOS = End Of Sentence

--- src/net/zero918nobita/Aquamarine/TokenType.java
+++ src/net/zero918nobita/Aquamarine/TokenType.java
@@ -4,5 +4,6 @@ package net.zero918nobita.Aquamarine;
  * Created by 0918nobita on 2016/03/10.
  */
 public class TokenType {
+    public static final int EOS = -1;
     public static final int INT = 257;
 }
