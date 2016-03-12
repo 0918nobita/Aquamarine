ccddf95bf9a74eb4926fa7e2de599742f2245933
0918nobita <nobita.0918@gmail.com>
2016-03-12 15:58:44 +0900

    Add JTInt class that extends JTCode class This is class that stores an integer.

---
+++ src/net/zero918nobita/Aquamarine/JTInt.java
@@ -0,0 +1,16 @@
+package net.zero918nobita.Aquamarine;
+
+/**
+ * Created by 0918nobita on 2016/03/12.
+ */
+public class JTInt extends JTCode {
+    private int value;
+
+    public JTInt(Integer integer) {
+        value = integer.intValue();
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
