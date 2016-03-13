2d26e192a6b7c9c8160290a1ad583180ead7cf5d
0918nobita <nobita.0918@gmail.com>
2016-03-13 09:22:43 +0900

    Add run(), add(), sub(), multiply(), and divide() method JTCode class shows a syntax tree.

--- src/net/zero918nobita/Aquamarine/JTCode.java
+++ src/net/zero918nobita/Aquamarine/JTCode.java
@@ -4,4 +4,23 @@ package net.zero918nobita.Aquamarine;
  * Created by 0918nobita on 2016/03/12.
  */
 public abstract class JTCode {
+    public JTCode run() throws Exception {
+        return this;
+    }
+
+    public JTCode add(JTCode code) throws Exception {
+        throw new Exception("このオブジェクトに演算子'＋'は使えません");
+    }
+
+    public JTCode sub(JTCode code) throws Exception {
+        throw new Exception("このオブジェクトに演算子'－'は使えません");
+    }
+
+    public JTCode multiply(JTCode code) throws Exception {
+        throw new Exception("このオブジェクトに演算子'＊'は使えません");
+    }
+
+    public JTCode devide(JTCode code) throws Exception {
+        throw new Exception("このオブジェクトに演算子'／'は使えません");
+    }
 }
