8522de9fa471c3d8e6c247dde5f7eaf60f406e0e
0918nobita <nobita.0918@gmail.com>
2016-03-13 09:30:41 +0900

    Improve readability

--- src/net/zero918nobita/Aquamarine/JTInt.java
+++ src/net/zero918nobita/Aquamarine/JTInt.java
@@ -4,18 +4,18 @@ package net.zero918nobita.Aquamarine;
  * Created by 0918nobita on 2016/03/12.
  */
 public class JTInt extends JTCode {
-    private int value;
+    private int value; // 数値
 
     public JTInt(Integer integer) {
         value = integer.intValue();
     }
 
-    public int getValue() {
+    public int getValue() { // 記憶している数値を返す
         return value;
     }
 
     @Override
-    public String toString() {
+    public String toString() { // 数値を文字列に変換したものを返す
         return Integer.toString(value);
     }
 }
