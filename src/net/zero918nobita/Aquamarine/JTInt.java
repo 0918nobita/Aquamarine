081df9b1b8b3aea7de81b075bb95989b067db8d4
0918nobita <nobita.0918@gmail.com>
2016-03-13 10:41:02 +0900

    Enable to calculate between JTInt objects I added new constructor, add(), sub(), multiply(), and divide() method.

--- src/net/zero918nobita/Aquamarine/JTInt.java
+++ src/net/zero918nobita/Aquamarine/JTInt.java
@@ -10,6 +10,10 @@ public class JTInt extends JTCode {
         value = integer.intValue();
     }
 
+    public JTInt(int i) {
+        value = i;
+    }
+
     public int getValue() { // 記憶している数値を返す
         return value;
     }
@@ -17,5 +21,49 @@ public class JTInt extends JTCode {
     @Override
     public String toString() { // 数値を文字列に変換したものを返す
         return Integer.toString(value);
+    }
+
+    @Override
+    public JTCode add(JTCode code) throws Exception {
+        JTCode result = null;
+        if (code.getClass() != JTInt.class) { // codeが数値かチェックする
+            throw new Exception("数値以外のものを足そうとしました");
+        }
+        JTInt i = (JTInt)code;
+        result = new JTInt(value + i.getValue()); // 足した値で新しいJTIntのオブジェクトを作成
+        return result;
+    }
+
+    @Override
+    public JTCode sub(JTCode code) throws Exception {
+        JTCode result = null;
+        if (code.getClass() != JTInt.class) {
+            throw new Exception("数値以外のものを引こうとしました");
+        }
+        JTInt i = (JTInt)code;
+        result = new JTInt(value - i.getValue());
+        return result;
+    }
+
+    @Override
+    public JTCode multiply(JTCode code) throws Exception {
+        JTCode result = null;
+        if (code.getClass() != JTInt.class) {
+            throw new Exception("数値外のものを掛けようとしました");
+        }
+        JTInt i = (JTInt)code;
+        result = new JTInt(value * i.getValue());
+        return result;
+    }
+
+    @Override
+    public JTCode devide(JTCode code) throws Exception {
+        JTCode result = null;
+        if (code.getClass() != JTInt.class) {
+            throw new Exception("数値以外のものを割ろうとしました");
+        }
+        JTInt i = (JTInt)code;
+        result = new JTInt(value / i.getValue());
+        return result;
     }
 }
